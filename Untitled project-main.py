8d0cf5c4a6044d4173151aa46d793974aab3759d
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-01-09 13:32:04 +0100

    Add files via upload

---
+++ Untitled project-main.py
@@ -0,0 +1,10 @@
+# Imports go at the top
+from microbit import *
+
+# Variable nombre
+nombre = "Maria"
+
+while True:
+    if button_a.was_pressed():
+        nombre    
+        display.scroll(nombre)        
