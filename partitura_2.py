7e2fb87a74a4cc8d1aad89284202dd64bfe6ea4d
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-02-09 19:36:39 +0100

    Create partitura_2.py

---
+++ partitura_2.py
@@ -0,0 +1,9 @@
+# Imports go at the top
+from microbit import *
+import music
+
+notas = ['A4:4','A4:4','A4:4','F4:6','C5:4','A4:4','F4:6','C5:4','A4:8','E5:4','E5:4',
+        'E5:4','F5:6','C5:4','A4b:4','F4:6','C5:4','A4:8','A5:4','A4:6','A4:4','A5:4',
+         'G5#:6','G5:4','F5#:4','E5#:4','F5:4',' ','A4#:2','D5#:4']
+
+music.play(notas)
