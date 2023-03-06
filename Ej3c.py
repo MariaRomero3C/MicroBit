4997a05595a33f840f9d25051dd53088c48312bf
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-03-06 13:40:26 +0100

    Create Ej3c.py

---
+++ Ej3c.py
@@ -0,0 +1,19 @@
+# Imports go at the top
+from microbit import *
+import music
+
+i = 0
+sonidos = ['b7:20','a7:20','g7:20','f7:20']
+
+while True:
+    if button_a.get_presses():
+        i = i+1
+    elif button_b.get_presses():
+        i = i-1
+    if i>len(sonidos):
+        i = 1
+    elif i<0:
+        i = len(sonidos)-1
+    else:
+        music.play(sonidos[i])
+        sleep(500)
