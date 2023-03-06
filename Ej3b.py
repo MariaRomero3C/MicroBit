44de0bd93fff3b680ab6a2684d71a49197d6eaf3
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-03-06 13:30:54 +0100

    Update Ej3b.py

--- Ej3b.py
+++ Ej3b.py
@@ -2,7 +2,7 @@
 from microbit import *
 
 i = 0
-imagenes = [Image.ARROW_E,Image.ARROW_N,Image.ARROW_NE,Image.ARROW_NW]
+imagenes = [Image.ARROW_E,Image.ARROW_N,Image.ARROW_W,Image.ARROW_S,Image.ARROW_NE,Image.ARROW_NW,Image.ARROW_SE,Image.ARROW_SW]
 
 while True:
     if button_a.get_presses():
@@ -10,8 +10,9 @@ while True:
     elif button_b.get_presses():
         i = i-1
     if i>len(imagenes):
-        i = 0
+        i = 1
     elif i<0:
         i = len(imagenes)-1
     else:
         display.show(imagenes[i])
+        sleep(500)
